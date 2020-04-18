316ebf027aed28b14d99e392723674d0f18bbb85
Technikdroide <heller-alex@gmx.de>
2020-04-18 05:29:59 +0200

    Commit #1

---
+++ gatekeeper.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="gatekeeper.css">
+    <title>Gatekeeper</title>
+  </head>
+  <body>
+
+  <ul class="navigation">
+    <li><a href="#"><font face="verdana">Support</font></a></li>
+    <li><a href="#"><font face="verdana">Logout</font></a></li>
+
+  </ul>
+
+
+  <div class ="gatekeeper">
+    <div class ="login_textarea">
+      <input type="text" placeholder="Login" name="login_cred"/>
+    </div>
+    <div class ="pw_textarea">
+      <input type="password" placeholder="Password" name="password_cred"/>
+    </div>
+    <div class ="confirm_button">
+      <button class="button">Confirm</button>
+    </div>
+  </div>
+
+
+
+  </body>
+</html>
